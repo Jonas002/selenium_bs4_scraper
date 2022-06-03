f55d3c9f27dca8f37a6f7b8c0c472bfd1a0c502c
Ubuntu <azureuser@main.atwqbvxbvldebnci3jufj1wnvd.fx.internal.cloudapp.net>
2022-06-03 05:24:44 +0000

    updated Scraper

--- Price_Scrapper.py
+++ Price_Scrapper.py
@@ -15,7 +15,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-# Import environment variables
+# Import libraries for environment variables
 import os
 from dotenv import load_dotenv
 load_dotenv()
